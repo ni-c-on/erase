7882b423d36223e083afd260adc807d1d54c1391
nicon <nicon.work@gmail.com>
2015-12-23 14:32:56 +0400

    add options

--- erase.php
+++ erase.php
@@ -7,26 +7,74 @@
  */
 
 if ($argc < 2) {
-    error_log('usage: php erase.php path/to/folder/ [days]');
+    error_log("\nusage: php erase.php [options] path/to/folder/\n\nOptions:\n\t-v\t\tVerbose\n\t-a=age\t\tAge of old files (days)");
     exit(1);
 }
 
-$folder = $argv[1];
-$age = isset($argv[2]) ? $argv[2] : 30; // days
+array_shift($argv);
+
+$folder = '';
+$verbose = false;
+$age = 30; // days
+
+foreach ($argv as $arg) {
+    $arg = explode('=', $arg, 2);
+    switch ($arg[0]) {
+        case '-a':
+            if (!isset($arg[1])) {
+                error_log("Wrong value for age\n");
+                exit(1);
+            }
+            $age = $age[1];
+            break;
+
+        case '-v':
+            $verbose = true;
+            break;
+
+        default:
+            if (strpos($arg[0], '-') !== false) {
+                error_log("Undefined option {$arg[0]}\n");
+                exit(1);
+            }
+            $folder = $arg[0];
+            break;
+    }
+}
+
+if (!$folder) {
+    error_log("Directory not specified\n");
+    exit(1);
+}
+
+if (!is_dir($folder)) {
+    error_log("No such file or directory in $folder\n");
+    exit(1);
+}
 
 if (!$dir = opendir($folder)) {
-    error_log("Can't read directory $folder");
+    error_log("Can't read directory $folder\n");
     exit(1);
 }
 
 $date = strtotime("-{$age} days");
 
+$count = 0;
+
 while(($file = readdir($dir)) !== false) {
     if ($file == '.' or $file == '..') continue;
 
     if (filectime($folder . DIRECTORY_SEPARATOR . $file) < $date) {
+        if ($verbose) echo 'Delete file: ' . $folder . DIRECTORY_SEPARATOR . $file . "\n";
         unlink($folder . DIRECTORY_SEPARATOR . $file);
+        ++$count;
     }
+}
+
+if (!$count && $verbose) {
+    echo "No files deleted\n";
+} elseif ($verbose) {
+    echo "Deleted $count filse\n";
 }
 
 closedir($dir);
